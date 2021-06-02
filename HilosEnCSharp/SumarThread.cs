e3fbab86f4d46a94839ff7a610bad0419cf0096d
JuanMndzStgo <84043074+JuanMndzStgo@users.noreply.github.com>
2021-06-01 19:22:02 -0500

    hilos en c

---
+++ HilosEnCSharp/SumarThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HilosEnCSharp
+{
+    class SumarThread
+    {
+        public void sumar(Object parametros)
+        {
+            Thread current = Thread.CurrentThread;
+            Int32[] numeros = (Int32[])parametros;
+            Int32 resultado = numeros[0] + numeros[1];
+            Console.WriteLine("El hilo "+ current.Name + "durmiendo por " +
+                resultado + " segudos");
+            Thread.Sleep(resultado * 1000);
+            Console.WriteLine("El resultado de la suma es "
+                + numeros[0] + "+" + numeros[1] + " = "
+                + resultado);
+
+            Console.WriteLine("hilo " + current.Name + "finalizo su trabajo");
+        }
+
+    }
+}
